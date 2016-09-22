849be1b799cd3c457021b6a9ca61985046ef42d0
Mingcai SHEN <archsh@gmail.com>
2016-09-22 17:20:28 +0800

    Updated.

--- timefmt.go
+++ timefmt.go
@@ -1,6 +1,88 @@
+/*
+Implementation of Python's strftime and strptime in Go
+Example:
+    str, err := timefmt.Strftime(time.Now(), "%Y/%m/%d") // 2016/09/22
+Directives:
+    %a - Locale’s abbreviated weekday name
+    %A - Locale’s full weekday name
+    %b - Locale’s abbreviated month name
+    %B - Locale’s full month name
+    %c - Locale’s appropriate date and time representation
+    %d - Day of the month as a decimal number [01,31]
+    %H - Hour (24-hour clock) as a decimal number [00,23]
+    %I - Hour (12-hour clock) as a decimal number [01,12]
+    %j - Day of year
+    %m - Month as a decimal number [01,12]
+    %M - Minute as a decimal number [00,59]
+    %p - Locale’s equivalent of either AM or PM
+    %S - Second as a decimal number [00,61]
+    %U - Week number of the year
+    %w - Weekday as a decimal number
+    %W - Week number of the year
+    %x - Locale’s appropriate date representation
+    %X - Locale’s appropriate time representation
+    %y - Year without century as a decimal number [00,99]
+    %Y - Year with century as a decimal number
+    %Z - Time zone name (no characters if no time zone exists)
+Note that %c returns RFC1123 which is a bit different from what Python does
+*/
 package timefmt
 
-import "time"
+import (
+	"time"
+)
+
+var longDayNames = []string{
+	"Sunday",
+	"Monday",
+	"Tuesday",
+	"Wednesday",
+	"Thursday",
+	"Friday",
+	"Saturday",
+}
+
+var shortDayNames = []string{
+	"Sun",
+	"Mon",
+	"Tue",
+	"Wed",
+	"Thu",
+	"Fri",
+	"Sat",
+}
+
+var shortMonthNames = []string{
+	"---",
+	"Jan",
+	"Feb",
+	"Mar",
+	"Apr",
+	"May",
+	"Jun",
+	"Jul",
+	"Aug",
+	"Sep",
+	"Oct",
+	"Nov",
+	"Dec",
+}
+
+var longMonthNames = []string{
+	"---",
+	"January",
+	"February",
+	"March",
+	"April",
+	"May",
+	"June",
+	"July",
+	"August",
+	"September",
+	"October",
+	"November",
+	"December",
+}
 
 func Strftime(t time.Time, format string) (string, error) {
 
--- timefmt_test.go
+++ timefmt_test.go
@@ -1,1 +1,64 @@
 package timefmt
+
+import (
+	"testing"
+	"time"
+)
+
+var formats = []string{
+	"",
+	"",
+	"",
+	"",
+	"",
+	"",
+}
+var invalid_formats = []string{
+	"",
+	"",
+	"",
+	"",
+}
+
+func TestCheck(t *testing.T) {
+	for _, f := range formats {
+		if nil != Check(f) {
+			t.Errorf("'%s' Should be valid!\n", f)
+		}
+	}
+	for _, f := range invalid_formats {
+		if nil == Check(f) {
+			t.Errorf("'%s' Should be invalid!\n", f)
+		}
+	}
+}
+
+func TestStrftime(t *testing.T) {
+	var validate = func (tm time.Time, format string, result string) {
+		if s, e := Strftime(tm, format); e != nil || s != result {
+			t.Errorf("Strftime(/%v/, '%s') should return '%s' but not (%s) (%s)\n", tm, format, result, e, s)
+		}
+	}
+	tm := time.Unix(1474524266, 321)
+	validate(tm, "%Y-%m-%dT%H%M%S","/2016-09-22T14:04:26")
+}
+
+func TestStrptime(t *testing.T) {
+
+	var validate = func (val string, format string, result time.Time) {
+		if tm, e := Strptime(val, format); e != nil || tm != result {
+			t.Errorf("Strptime('%s', '%s') should return /%v/ but not (%s) (%v)\n", val, format, result, e, tm)
+		}
+	}
+	tm := time.Unix(1474524266, 321)
+	validate("2016-09-22T14:04:26.000000321", "%Y-%m-%dT%H%M%S.%f", tm)
+
+}
+
+func BenchmarkStrftime(b *testing.B) {
+
+}
+
+func BenchmarkStrptime(b *testing.B) {
+
+}
